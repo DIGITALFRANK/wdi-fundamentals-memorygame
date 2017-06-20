558add342ce670cbcbca6fa0c1f06402a7c66877
Francis Agbodji <Frankie@Francis-Agbodjis-Computer-2.local>
2017-06-20 16:51:43 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,5 +5,29 @@ var cardTwo = 'king';
 var cardThree = 'queen';
 var cardFour = 'king';
 
-console.log('User flipped ' + cardFour)
-console.log('User flipped ' + cardTwo)
+console.log('User flipped ' + cardFour);
+console.log('User flipped ' + cardTwo);
+
+
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("sorry, try again!");
+	}
+}
